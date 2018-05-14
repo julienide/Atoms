66087190f0e57c4926dac7ffaf07b490f7481565
JulienIde <julien.ide.fr@gmail.com>
2018-05-14 11:22:42 +0200

    BUG FIX: Error in the calculation of fractional coordinates

--- R/wrap.R
+++ R/wrap.R
@@ -1,7 +1,7 @@
 #' wrap
-#' 
+#'
 #' wrap
-#' 
+#'
 #' @name wrap
 #' @export
 wrap <- function(x, ...)
--- src/wrap.cpp
+++ src/wrap.cpp
@@ -9,10 +9,10 @@ void wrapAtoms(
     const Rcpp::LogicalVector& multi)
 {
   bool _multi = multi[0] ;
-  
+
   Rcpp::CharacterVector lvls = resnumb.attr("levels") ;
   int nlvls = lvls.size() ;
-  
+
   Rcpp::LogicalVector pbc = x.slot("pbc") ;
   Rcpp::NumericMatrix a = x.slot("a") ;
   Rcpp::NumericMatrix b = x.slot("b") ;
@@ -22,22 +22,22 @@ void wrapAtoms(
   Rcpp::NumericMatrix pz = x.slot("z") ;
   int nframe = px.ncol() ;
   int natom = px.nrow() ;
-  
+
   Rcpp::Environment baseEnv("package:base") ;
   Rcpp::Function solve = baseEnv["solve"] ;
-  
+
   Progress pb(nframe, _multi) ;
   for(int frame = 0; frame < nframe; frame++){
     if(Progress::check_abort()){
       Rcpp::stop("Interuption") ;
     }
-    
+
     Rcpp::NumericMatrix cell(3, 3) ;
     cell(0, 0) = a(0, frame) ; cell(0, 1) = b(0, frame) ; cell(0, 2) = c(0, frame) ;
     cell(1, 0) = a(1, frame) ; cell(1, 1) = b(1, frame) ; cell(1, 2) = c(1, frame) ;
     cell(2, 0) = a(2, frame) ; cell(2, 1) = b(2, frame) ; cell(2, 2) = c(2, frame) ;
     Rcpp::NumericMatrix cellInv = solve(cell) ;
-    
+
     std::vector< double > cx(nlvls, 0.0) ;
     std::vector< double > cy(nlvls, 0.0) ;
     std::vector< double > cz(nlvls, 0.0) ;
@@ -55,30 +55,30 @@ void wrapAtoms(
       cx[res] = cx[res]/cn[res] ;
       cy[res] = cy[res]/cn[res] ;
       cz[res] = cz[res]/cn[res] ;
-      cx[res] = cx[res]*cellInv(0, 0) + cy[res]*cellInv(0, 1) + cz[res]*cellInv(0, 2) ;
-      cy[res] = cx[res]*cellInv(1, 0) + cy[res]*cellInv(1, 1) + cz[res]*cellInv(1, 2) ;
-      cz[res] = cx[res]*cellInv(2, 0) + cy[res]*cellInv(2, 1) + cz[res]*cellInv(2, 2) ;
+      double cxf = cx[res]*cellInv(0, 0) + cy[res]*cellInv(0, 1) + cz[res]*cellInv(0, 2) ;
+      double cyf = cx[res]*cellInv(1, 0) + cy[res]*cellInv(1, 1) + cz[res]*cellInv(1, 2) ;
+      double czf = cx[res]*cellInv(2, 0) + cy[res]*cellInv(2, 1) + cz[res]*cellInv(2, 2) ;
       if(pbc[0])
       {
-        cx[res] = -std::floor(cx[res]) ;
+        cxf = -std::floor(cxf) ;
       } else {
-        cx[res] = 0.0 ;
+        cxf = 0.0 ;
       }
       if(pbc[1])
       {
-        cy[res] = -std::floor(cy[res]) ;
+        cyf = -std::floor(cyf) ;
       } else {
-        cy[res] = 0.0 ;
+        cyf = 0.0 ;
       }
       if(pbc[2])
       {
-        cz[res] = -std::floor(cz[res]) ;
+        czf = -std::floor(czf) ;
       } else {
-        cz[res] = 0.0 ;
+        czf = 0.0 ;
       }
-      cx[res] = cx[res]*cell(0, 0) + cy[res]*cell(0, 1) + cz[res]*cell(0, 2) ;
-      cy[res] = cx[res]*cell(1, 0) + cy[res]*cell(1, 1) + cz[res]*cell(1, 2) ;
-      cz[res] = cx[res]*cell(2, 0) + cy[res]*cell(2, 1) + cz[res]*cell(2, 2) ;
+      cx[res] = cxf*cell(0, 0) + cyf*cell(0, 1) + czf*cell(0, 2) ;
+      cy[res] = cxf*cell(1, 0) + cyf*cell(1, 1) + czf*cell(1, 2) ;
+      cz[res] = cxf*cell(2, 0) + cyf*cell(2, 1) + czf*cell(2, 2) ;
     }
     for(int atom = 0; atom < natom; atom++)
     {
